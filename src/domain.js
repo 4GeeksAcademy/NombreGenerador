9a4ff630b7e4afb08313fd87211ef39cbc152b5a
EuclidesT <94712951+EuclidesT@users.noreply.github.com>
2024-08-10 14:27:09 +0000

    terminado

--- src/domain.js
+++ src/domain.js
@@ -1,23 +1,28 @@
 let pronoun = ["the", "our"];
 let adjetivo = ["great", "big"];
-let sustantivo = ["jogg", "racoon"];
+let sustantivo = ["jogg", "racoon", "arboles"];
+let extensiones = [".com", ".net", ".us", ".io", ".es"];
 
 pronoun.forEach((articulo) => {
   adjetivo.forEach((adje) => {
     sustantivo.forEach((sustan) => {
-      console.log(articulo + adje + sustan + ".com");
-    });
-  });
-});
+      extensiones.forEach((exte) => {
+        const nombre = sustan;
+        const extension = exte;
+        const ultimasLetras = nombre.slice(-1 * (extension.length - 1));
+        const extensionSimple = extension.slice(1);
 
-let nombre = ["the", "our"];
-let apellido = ["excelent", "flor"];
-let ciudad = ["profesor", "español"];
-
-nombre.forEach((nom) => {
-  apellido.forEach((apell) => {
-    ciudad.forEach((ciud) => {
-      console.log(nom + apell + ciud + ".es");
+        if (nombre.endsWith(extensionSimple)) {
+          console.log(
+            articulo +
+              adje +
+              nombre.slice(0, nombre.length - ultimasLetras.length) +
+              extension
+          );
+        } else {
+          console.log(articulo + adje + sustan + exte);
+        }
+      });
     });
   });
 });
